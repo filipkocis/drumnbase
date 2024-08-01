b44bff7fd261f35c7c5c58d651f6397dd6d7f651
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-02 00:40:06 +0200

    implement column struct

---
+++ src/basics/column.rs
@@ -0,0 +1,55 @@
+pub enum TextType {
+    Char,
+    Variable,
+    Fixed(u32),
+}
+
+pub enum NumericType {
+    IntU8,
+    IntU16,
+    IntU32,
+    IntU64,
+
+    IntI8,
+    IntI16,
+    IntI32,
+    IntI64,
+
+    Float32,
+    Float64,
+}
+
+pub enum TimestampType {
+    Date,
+    Time,
+    DateTime,
+    TimeStamp,
+}
+
+
+pub enum ColumnType {
+    Numeric(NumericType),
+    Text(TextType),
+    Timestamp(TimestampType),
+    Boolean,
+    Binary,
+    Array,
+    Enum,
+    UUID,
+}
+
+pub struct Column {
+    pub name: String,
+    pub data_type: ColumnType,
+    pub length: u32,
+    pub default: String,
+    pub not_null: bool,
+    pub unique: bool,
+    pub read_only: bool,
+    // pub primary_key: bool,
+    // pub foreigh_key: bool,
+    // pub check: bool,
+    // pub references: String,
+    // pub check_constraint: String,
+    // pub privileges: Vec<Privilege>,
+}
