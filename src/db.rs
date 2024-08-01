063966c1ccb0d278a439441a3491a5e9a25abfe6
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-02 00:39:45 +0200

    implement database struct

---
+++ src/db.rs
@@ -0,0 +1,19 @@
+use crate::basics::table::Table;
+
+
+pub struct DataBase {
+    pub name: String,    
+    pub tables: Vec<Table>,
+    // pub views: Vec<View>,
+    // pub functions: Vec<Function>,
+    // pub procedures: Vec<Procedure>,
+    // pub triggers: Vec<Trigger>,
+    // pub indexes: Vec<Index>,
+    // pub sequences: Vec<Sequence>,
+    // pub roles: Vec<Role>,
+    // pub users: Vec<User>,
+    // pub groups: Vec<Group>,
+    // pub privileges: Vec<Privilege>,
+    // pub constraints: Vec<Constraint>,
+    // pub schemas: Vec<Schema>,
+}
