ab7686caf06502133b6917c42c0e27ab64cf8d21
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-03 01:34:10 +0200

    feat(table): implement basic functions

--- src/basics/table.rs
+++ src/basics/table.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use crate::{basics::column::Column, file::data::Data};
 
 pub struct Table {
@@ -12,4 +14,25 @@ pub struct Table {
     // pub partitions: Vec<Partition>,
     // pub comments: Vec<Comment>,
     // pub privileges: Vec<Privilege>,
+}
+
+impl Table {
+    pub fn new(path: &str) -> Table {
+        let path = PathBuf::from(path);
+
+        Table {
+            name: path.file_stem().unwrap().to_str().unwrap().to_owned(),
+            columns: Vec::new(),
+            data: Data::new(path),
+            read_only: false,
+        }
+    }
+
+    pub fn get_column_mut(&mut self, column_name: &str) -> Option<&mut Column> {
+        self.columns.iter_mut().find(|column| column.name == column_name)
+    }
+
+    pub fn get_column(&self, column_name: &str) -> Option<&Column> {
+        self.columns.iter().find(|column| column.name == column_name)
+    }
 }
