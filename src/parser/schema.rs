2d7d2284d25dc417bf6fea46251d3b8fcf474c40
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-03 01:37:43 +0200

    feat(schema): implement schema WIP

---
+++ src/parser/schema.rs
@@ -0,0 +1,46 @@
+use crate::basics::{table::Table, column::Column};
+
+pub struct Schema {
+    pub root_dir: String,
+    pub tables: Vec<Table>,
+}
+
+impl Schema {
+    pub fn new(root_dir: String) -> Schema {
+        Schema {
+            root_dir,
+            tables: Vec::new(),
+        }
+    }
+
+    pub fn add_table(&mut self, name: &str) {
+        let table = Table::new(name);
+
+        self.tables.push(table);
+    }
+
+    pub fn delete_table(&mut self, name: &str) {
+        self.tables.retain(|table| table.name != name);
+    }
+
+    pub fn get_table(&mut self, name: &str) -> Option<&mut Table> {
+        self.tables.iter_mut().find(|table| table.name == name)
+    }
+
+    pub fn get_column(&mut self, table_name: &str, column_name: &str) -> Option<&mut Column> {
+        self.get_table(table_name)
+            .and_then(|table| table.columns
+                .iter_mut()
+                .find(|column| column.name == column_name)
+            )
+    }
+}
+
+impl Default for Schema {
+    fn default() -> Self {
+        Schema {
+            root_dir: String::new(),
+            tables: Vec::new(),
+        }
+    }
+}
