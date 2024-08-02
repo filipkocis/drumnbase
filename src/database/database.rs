32797fc5163d3257d5adc1c9100112ffd2e47040
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-03 01:35:37 +0200

    refactor(db): move and rename db.rs

--- src/database/database.rs
+++ src/database/database.rs
@@ -1,9 +1,10 @@
 use crate::basics::table::Table;
 
 
-pub struct DataBase {
+pub struct Database {
     pub name: String,    
     pub tables: Vec<Table>,
+    pub root_dir: String,
     // pub views: Vec<View>,
     // pub functions: Vec<Function>,
     // pub procedures: Vec<Procedure>,
@@ -16,4 +17,20 @@ pub struct DataBase {
     // pub privileges: Vec<Privilege>,
     // pub constraints: Vec<Constraint>,
     // pub schemas: Vec<Schema>,
+}
+
+impl Default for Database {
+    fn default() -> Self {
+        Database {
+            name: String::from(""),
+            tables: vec![],
+            root_dir: String::from("data"),
+        }
+    } 
+}
+
+impl Database {
+    pub fn path(&self) -> String {
+        format!("{}/{}", self.root_dir, self.name) 
+    }
 }
