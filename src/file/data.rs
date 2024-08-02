d71af1f348b4b9015e23a88e6c3fdba9b324d0fa
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-03 01:33:54 +0200

    feat(data): implement data new fn

--- src/file/data.rs
+++ src/file/data.rs
@@ -14,3 +14,21 @@ pub struct Data {
     pub path: PathBuf,
     pub load_mode: LoadMode,
 }
+
+impl Data {
+    pub fn new(path: PathBuf) -> Data {
+        let writer_file = File::options().write(true).create(true).open(&path).unwrap();
+        let reader_file = File::options().read(true).open(&path).unwrap();
+
+        let reader = BufReader::new(reader_file);
+        let writer = BufWriter::new(writer_file);
+
+        Data {
+            rows: Vec::new(),
+            reader,
+            writer,
+            path,
+            load_mode: LoadMode::Memory,
+        }
+    } 
+}
