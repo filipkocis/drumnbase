e5cce5360af7d56586cfb37b61ac342d5da77dc3
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-04 00:43:55 +0200

    refactor: remove unused old code

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,6 +1,5 @@
 pub mod database;
 pub mod new;
-pub mod load;
 pub mod builder;
 
 use database::Database;
--- src/database/new.rs
+++ src/database/new.rs
@@ -1,5 +1,4 @@
 use crate::database::Database;
-use crate::utils::{disk, log};
 use crate::parser::Schema;
 
 impl Database {
@@ -9,24 +8,5 @@ impl Database {
             tables: schema.tables,
             root_dir: root_dir.to_string(),
         }
-    }
-
-    fn create_files(&self) {
-        log::info(format!("creating database files '{}'", self.name));
-        let path = self.path();
-
-        if disk::exists(&path) {
-            log::error(format!("database '{}' already exists", self.name));
-            return;
-        }
-
-        disk::create_directory_all(&path);
-        disk::create_directory(&format!("{}/tables", &path));
-
-        for table in &self.tables {
-            disk::create_file(&format!("{}/tables/{}.quack", &path, &table.name));
-        }
-
-        log::success(format!("database '{}' created", self.name));
     }
 }
