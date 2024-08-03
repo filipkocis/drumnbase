f109f126f35cf947d20beda9439108937bfdd58b
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-03 19:12:25 +0200

    feat(database): implement new_from_schema

--- src/database/new.rs
+++ src/database/new.rs
@@ -1,25 +1,34 @@
-use crate::basics::table::Table;
 use crate::database::Database;
 use crate::utils::{disk, log};
 use crate::parser::Schema;
 
 impl Database {
-    pub fn new_from_schema(name: String, schema: Schema) -> Self {
-        let database = Database {
-            name,
+    pub fn new_from_schema(schema: Schema) -> Self {
+        log::info(format!("loading database '{}' from schema", schema.database_name));
+
+        let mut database = Database {
+            name: schema.database_name,
             tables: schema.tables,
             root_dir: schema.root_dir,
         };
 
         database.create_files();
+        database.load_tables();
+
         database
     }
 
+    fn load_tables(&mut self) {
+        let path = format!("{}/tables", self.path());
+        self.tables.iter_mut().for_each(|table| table.load(&path))
+    }
+
     fn create_files(&self) {
+        log::info(format!("creating database files '{}'", self.name));
         let path = self.path();
 
         if disk::exists(&path) {
-            log::error(format!("database {} already exists", self.name));
+            log::error(format!("database '{}' already exists", self.name));
             return;
         }
 
@@ -27,7 +36,9 @@ impl Database {
         disk::create_directory(&format!("{}/tables", &path));
 
         for table in &self.tables {
-            disk::create_directory(&format!("{}/tables/{}.quack", &path, &table.name));
+            disk::create_file(&format!("{}/tables/{}.quack", &path, &table.name));
         }
+
+        log::success(format!("database '{}' created", self.name));
     }
 }
