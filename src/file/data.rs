1ed825edd9688144cbb788a58d08ac591b3f4df2
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-03 18:15:53 +0200

    feat(data): implement Default, prevent file creation on new()

--- src/file/data.rs
+++ src/file/data.rs
@@ -11,26 +11,48 @@ pub enum LoadMode {
 #[derive(Debug)]
 pub struct Data {
     pub rows: Vec<Row>,
-    pub reader: BufReader<File>,
-    pub writer: BufWriter<File>,
-    pub path: PathBuf,
+    reader: Option<BufReader<File>>,
+    writer: Option<BufWriter<File>>,
+    path: Option<PathBuf>,
+    loaded: bool,
     pub load_mode: LoadMode,
 }
 
 impl Data {
-    pub fn new(path: PathBuf) -> Data {
+    pub fn load(&mut self, path: PathBuf) {
+        if self.loaded { return }
+
         let writer_file = File::options().write(true).create(true).open(&path).unwrap();
         let reader_file = File::options().read(true).open(&path).unwrap();
 
         let reader = BufReader::new(reader_file);
         let writer = BufWriter::new(writer_file);
 
+        self.reader = Some(reader);
+        self.writer = Some(writer);
+        self.path = Some(path);
+
+        self.loaded = true;
+    }
+
+    pub fn new(path: PathBuf) -> Data {
+        let mut data = Data::default();
+        data.load(path); 
+
+        data
+    } 
+}
+
+impl Default for Data {
+    fn default() -> Self {
         Data {
             rows: Vec::new(),
-            reader,
-            writer,
-            path,
             load_mode: LoadMode::Memory,
+
+            reader: None,
+            writer: None,
+            path: None,
+            loaded: false,
         }
-    } 
+    }
 }
