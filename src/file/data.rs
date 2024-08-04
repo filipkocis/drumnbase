615938e0dbc3e13fab9d64cd7cbcbb52471daa39
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-04 22:08:08 +0200

    feat(data): implement writing rows to mem and disk

--- src/file/data.rs
+++ src/file/data.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, io::{BufReader, BufWriter}, fs::File};
+use std::{path::PathBuf, io::{BufReader, BufWriter, Write}, fs::File};
 
 use crate::{basics::row::Row, utils::log};
 
@@ -10,7 +10,8 @@ pub enum LoadMode {
 
 #[derive(Debug)]
 pub struct Data {
-    pub rows: Vec<Row>,
+    pub buf_rows: Vec<Row>,
+    rows: Vec<Row>,
     reader: Option<BufReader<File>>,
     writer: Option<BufWriter<File>>,
     path: Option<PathBuf>,
@@ -43,12 +44,53 @@ impl Data {
 
         data
     } 
+
+    pub fn is_empty(&self) -> bool {
+        self.rows.is_empty()
+    }
+
+    pub fn is_empty_buf(&self) -> bool {
+        self.buf_rows.is_empty()
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<Row> {
+        self.rows.iter()
+    }
+}
+
+impl Data {
+    pub fn write_memory(&mut self) -> Result<(), String> {
+        if self.buf_rows.len() == 0 { return Ok(()) }
+        if !self.loaded { return Err("data not loaded".to_string()) }
+
+        self.buf_rows.iter().rev().for_each(|r| {
+            self.rows.push(r.clone())
+        });
+
+        self.write_disk()
+    }
+
+    pub fn write_disk(&mut self) -> Result<(), String> {
+        if self.buf_rows.len() == 0 { return Ok(()) }
+        if !self.loaded { return Err("data not loaded".to_string()) }
+
+        let writer = self.writer.as_mut().unwrap();
+
+        self.buf_rows.reverse();
+        for buf_row in self.buf_rows.pop() {
+            writer.write_all(buf_row.to_string().as_bytes()).unwrap();
+        }
+
+        writer.flush().unwrap();
+        Ok(())
+    }
 }
 
 impl Default for Data {
     fn default() -> Self {
         Data {
             rows: Vec::new(),
+            buf_rows: Vec::new(),
             load_mode: LoadMode::Memory,
 
             reader: None,
