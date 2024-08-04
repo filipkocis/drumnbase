d381c0d54fd20caa8cacb801286e8b1b5388bc4f
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-05 00:55:40 +0200

    refactor(writer): move trait impl from data to table

--- src/file/write.rs
+++ src/file/write.rs
@@ -1,7 +1,5 @@
 use crate::{parser::Schema, basics::table::Table, file::data::LoadMode};
 
-use super::data::Data;
-
 pub trait DatabaseWriter {
     fn write(&mut self) -> Result<(), String>;
 }
@@ -18,15 +16,9 @@ impl DatabaseWriter for Schema {
 
 impl DatabaseWriter for Table {
     fn write(&mut self) -> Result<(), String> {
-        self.data.write()
-    }
-}
-
-impl DatabaseWriter for Data {
-    fn write(&mut self) -> Result<(), String> {
-        match self.load_mode {
-            LoadMode::Memory => self.write_memory(),
-            LoadMode::Disk => self.write_disk(),
+        match self.data.load_mode {
+            LoadMode::Memory => self.data.write_memory(&self.columns),
+            LoadMode::Disk => self.data.write_disk(&self.columns),
         }
     }
 }
