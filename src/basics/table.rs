2b5a2918992f481b5c85984865f6d31033997691
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-04 17:53:15 +0200

    feat(table): implement print functions

--- src/basics/table.rs
+++ src/basics/table.rs
@@ -51,6 +51,54 @@ impl Table {
     }
 }
 
+impl Table {
+    pub fn print(&self) {
+        let separator = "=".repeat(50);
+        println!("{}", separator);
+        self.print_info();
+        println!();
+        self.print_columns();
+        println!();
+        self.print_rows();
+        println!("{}", separator);
+    }
+
+    pub fn print_info(&self) {
+        println!("TABLE NAME: '{}'", self.name);
+        println!("LOAD MODE: {:?}", self.data.load_mode);
+        println!("READ ONLY: {}", self.read_only);
+    }
+
+    pub fn print_columns(&self) {
+        for column in &self.columns {
+            print!("COLUMN '{}' TYPE '{:?}' ", column.name, column.data_type);
+            if !column.default.is_empty() { print!("DEFAULT='{}' ", column.default); }
+            if column.not_null { print!("NOTNULL "); }
+            if column.unique { print!("UNIQUE "); }
+            if column.read_only { print!("READONLY "); }
+            println!();
+        }
+    }
+
+    pub fn print_rows(&self) {
+        let spacing = 7;
+
+        if !self.data.rows.is_empty() {
+            print!("     ");
+            print!("{}", self.columns.iter().map(|c| format!("{:width$} ", c.name.to_uppercase(), width = spacing)).collect::<String>());
+            println!();
+        }
+
+        for row in &self.data.rows {
+            print!("ROW: ");
+            for cell in row.iter() {
+                print!("{:width$} ", format!("{}", cell), width = spacing);
+            }
+            println!();
+        }
+    }
+}
+
 impl Default for Table {
     fn default() -> Self {
         Table {
