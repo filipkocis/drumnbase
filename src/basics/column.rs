717733f609045ce7e070283ac64d6c6d4f49cd2b
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-04 14:47:39 +0200

    feat(column_type): implement validation and parsing

--- src/basics/column.rs
+++ src/basics/column.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+
+use super::row::{Value, NumericValue};
+
 #[derive(Debug)]
 pub enum TextType {
     Char,
@@ -42,6 +46,82 @@ pub enum ColumnType {
     UUID,
 }
 
+impl NumericType {
+    pub fn parse(&self, value: &str) -> Result<Value, String> {
+        match self {
+            NumericType::IntI8 => value.parse::<i8>().map(|v| Value::Numeric(NumericValue::IntI8(v))).map_err(|e| e.to_string()), 
+            NumericType::IntI16 => value.parse::<i16>().map(|v| Value::Numeric(NumericValue::IntI16(v))).map_err(|e| e.to_string()), 
+            NumericType::IntI32 => value.parse::<i32>().map(|v| Value::Numeric(NumericValue::IntI32(v))).map_err(|e| e.to_string()), 
+            NumericType::IntI64 => value.parse::<i64>().map(|v| Value::Numeric(NumericValue::IntI64(v))).map_err(|e| e.to_string()), 
+            
+            NumericType::IntU8 => value.parse::<u8>().map(|v| Value::Numeric(NumericValue::IntU8(v))).map_err(|e| e.to_string()), 
+            NumericType::IntU16 => value.parse::<u16>().map(|v| Value::Numeric(NumericValue::IntU16(v))).map_err(|e| e.to_string()), 
+            NumericType::IntU32 => value.parse::<u32>().map(|v| Value::Numeric(NumericValue::IntU32(v))).map_err(|e| e.to_string()), 
+            NumericType::IntU64 => value.parse::<u64>().map(|v| Value::Numeric(NumericValue::IntU64(v))).map_err(|e| e.to_string()), 
+
+            NumericType::Float32 => value.parse::<f32>().map(|v| Value::Numeric(NumericValue::Float32(v))).map_err(|e| e.to_string()), 
+            NumericType::Float64 => value.parse::<f64>().map(|v| Value::Numeric(NumericValue::Float64(v))).map_err(|e| e.to_string()), 
+        } 
+    }
+}
+
+impl TextType {
+    pub fn parse(&self, value: &str) -> Result<Value, String> {
+        match self {
+            TextType::Char => {
+                if value.len() != 1 {
+                    return Err(format!("Invalid char length {}", value))
+                }
+            },
+            TextType::Fixed(v) => {
+                if value.len() > *v as usize {
+                    return Err(format!("Invalid fixed text length {}, should be {}", value, v))
+                }
+            }
+            TextType::Variable => { todo!() }
+        }; 
+
+        Ok(Value::Text(value.to_string()))
+    }
+}
+
+impl TimestampType {
+    pub fn parse(&self, value: &str) -> Result<Value, String> {
+        todo!()
+    }
+}
+
+impl ColumnType {
+    pub fn parse(&self, value: &str) -> Result<Value, String> {
+        match self {
+            ColumnType::Numeric(t) => t.parse(value),
+            ColumnType::Text(t) => t.parse(value),
+            ColumnType::Timestamp(t) => t.parse(value),
+
+            ColumnType::Boolean => value.parse::<bool>().map(Value::Boolean).map_err(|e| e.to_string()),
+            ColumnType::Binary => value.parse::<Bytes>().map(|v| Value::Binary(v.0)).map_err(|e| e.to_string()),
+            ColumnType::Array => todo!(),
+            ColumnType::Enum => todo!(),
+            ColumnType::UUID => todo!(),
+        }
+    }
+}
+
+struct Bytes(Vec<u8>);
+impl FromStr for Bytes {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut bytes = Vec::new(); 
+
+        for byte in s.split(",") {
+            bytes.push(u8::from_str_radix(byte, 2).map_err(|e| e.to_string())?);
+        }
+
+        Ok(Bytes(bytes))
+    }
+}
+
 #[derive(Debug)]
 pub struct Column {
     pub name: String,
@@ -70,5 +150,13 @@ impl Column {
             unique: false,
             read_only: false,
         }
+    }
+
+    pub fn set_length(&mut self, length: u32) {
+        self.length = length;
+    }
+
+    pub fn validate(&self, value: &str) -> Result<Value, String> {
+        self.data_type.parse(value)
     }
 }
