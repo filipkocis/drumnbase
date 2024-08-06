fdea8a8ca2d4100374b1576ba6254ec75b613902
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-07 00:40:57 +0200

    feat(query): implement value ordering and helpers

--- src/query/query.rs
+++ src/query/query.rs
@@ -8,6 +8,24 @@ pub struct QueryResult {
     pub data: Vec<Row>,
 }
 
+impl QueryResult {
+    pub fn from(data: Vec<Row>) -> Self {
+        Self {
+            amount: data.len(),
+            data
+        }
+    }
+}
+
+impl Default for QueryResult {
+    fn default() -> Self {
+        Self {
+            amount: 0,
+            data: vec![],
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum SelectExtra {
     Where(ConditionChain),
@@ -41,6 +59,53 @@ impl SelectExtra {
 pub struct SelectQuery {
     pub columns: Vec<String>,  
     pub extras: Vec<SelectExtra>,
+}
+
+impl SelectQuery {
+    pub fn get_limit(&self) -> Option<usize> {
+        for extra in &self.extras {
+            if let SelectExtra::Limit(n) = extra {
+                return Some(*n)
+            }
+        }
+        None
+    }
+
+    pub fn get_offset(&self) -> Option<usize> {
+        for extra in &self.extras {
+            if let SelectExtra::Offset(n) = extra {
+                return Some(*n)
+            }
+        }
+        None
+    }
+
+    pub fn get_order(&self) -> Option<&Order> {
+        for extra in &self.extras {
+            if let SelectExtra::Order(order) = extra {
+                return Some(order)
+            }
+        }
+        None
+    }
+
+    pub fn get_exclude(&self) -> Option<&Vec<String>> {
+        for extra in &self.extras {
+            if let SelectExtra::Exclude(cols) = extra {
+                return Some(cols)
+            }
+        }
+        None
+    }
+
+    pub fn get_where(&self) -> Option<&ConditionChain> {
+        for extra in &self.extras {
+            if let SelectExtra::Where(chain) = extra {
+                return Some(chain)
+            }
+        }
+        None
+    }
 }
 
 #[derive(Debug)]
@@ -78,6 +143,20 @@ pub enum Order {
     Descending(String),
 }
 
+impl Order {
+    /// Compare two rows based on the order, column should be valid and checked before calling this function
+    /// - this functin will panic if the column index is out of bounds or the comparisson is invalid
+    pub fn compare(&self, a: &Row, b: &Row, i: usize) -> std::cmp::Ordering {
+        let a = a.get(i).expect("Row index out of bounds, invalid column index");
+        let b = b.get(i).expect("Row index out of bounds, invalid column index");
+
+        match self {
+            Self::Ascending(_) => a.partial_cmp(&b).expect("Invalid comparison"),
+            Self::Descending(_) => b.partial_cmp(&a).expect("Invalid comparison"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum QueryType {
     Select(SelectQuery),
@@ -108,4 +187,5 @@ impl Query {
 
     pub fn get_text(&self) -> &str { &self.text }
     pub fn get_table_name(&self) -> &str { &self.table_name }
+    pub fn get_specific(&self) -> Option<&QueryType> { self.specific.as_ref() }
 }
