17cc2b703dabf6561fa2045ae305b3600c62311a
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-14 13:33:43 +0200

    feat(sync): implement read/write replacement WIP

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -1,3 +1,4 @@
 pub mod data;
 pub mod write;
 pub mod read;
+pub mod sync;
---
+++ src/file/sync.rs
@@ -0,0 +1,81 @@
+use std::io::SeekFrom;
+
+use crate::basics::{table::Table, row::ToBytes};
+
+use super::data::Data;
+
+impl Table {
+    pub fn get_row_prefix_length(&self) -> usize {
+        // TODO: in the future, this will include bits for the row's metadata
+        0     
+    }
+
+    pub fn get_row_length(&self) -> usize {
+        self.columns
+            .iter()
+            .fold(
+                self.get_row_prefix_length(), 
+                |acc, column| acc + column.length as usize
+            )
+    }
+
+    pub fn get_column_offset(&self, column_index: usize) -> Result<usize, String> {
+        if column_index >= self.columns.len() {
+            return Err(format!("Column index out of bounds: {}", column_index))
+        }
+
+        let mut offset = self.get_row_prefix_length();
+        for i in 0..column_index {
+            offset += self.columns[i].length as usize;
+        }
+
+        Ok(offset)
+    }
+
+    pub fn get_row_offset(&self, row_index: usize) -> Result<usize, String> {
+        if row_index >= self.data.len() {
+            return Err(format!("Row index out of bounds: {}", row_index))
+        }
+
+        Ok(row_index * self.get_row_length())
+    }
+}
+
+impl Table {
+    /// Syncs the row at the given index with the disk
+    pub fn sync_row(&mut self, index: usize) {
+        todo!()
+    }
+
+    /// Syncs the row at the given index with the disk, it only syncs the specified columns
+    pub fn sync_row_parts(&mut self, row_index: usize, column_indexes: &Vec<usize>) -> Result<(), String> {
+        let mut column_indexes = column_indexes.clone();
+        column_indexes.sort_unstable();
+        let row = self.data.get(row_index).unwrap();
+        let row_offset = self.get_row_offset(row_index)?;
+
+        let buffers_with_col_idx = column_indexes
+            .iter()
+            .map(|&column_index| {
+                let column_length = self.columns[column_index].length;
+                let buffer = row.get(column_index).unwrap().to_bytes(column_length);
+
+                (column_index, buffer)
+            })
+            .collect::<Vec<_>>();
+    
+        self.data.writer_seek(row_offset as u64)?;
+
+        // TODO
+        // columns are sorted, so we can iterate and seek through them in order
+        for (column_index, buffer) in buffers_with_col_idx {
+            let column_offset = self.get_column_offset(column_index)?;
+
+            self.data.writer_seek((row_offset + column_offset) as u64)?;
+            self.data.writer_write(&buffer)?;
+            // self.data.writer_flush()?;
+        }
+
+        Ok(())
+    }
+}
