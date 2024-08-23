b046567de80b43a9d562f0abce01584d54a4074f
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-08-23 15:57:47 +0200

    feat(parser): impl util functions

--- src/syntax/parser.rs
+++ src/syntax/parser.rs
@@ -14,6 +14,30 @@ impl Parser {
         Self { tokens, current: 0 }
     }
 
+    /// Expects the current token to be of the specified kind, consumes and returns it.
+    fn expect(&mut self, kind: TokenKind) -> Result<&Token, String> {
+        self.advance();
+        let token = self.previous();
+
+        match token {
+            Some(token) if token.kind == kind => Ok(token),
+            _ => Err(self.expected(kind))
+        }
+    }
+
+    fn expected(&self, expected: impl Debug) -> String {
+        if let Some(token) = self.current() {
+            format!("Expected {:?} but found {:?}", expected, token.kind)
+        } else {
+            format!("Expected {:?} but found None", expected)
+        }
+    }
+
+    /// Returns the current token, or an error if there is none.
+    fn current_token(&self) -> Result<&Token, &str> {
+        self.current().ok_or("No token found")
+    }
+
     fn current(&self) -> Option<&Token> {
         self.tokens.get(self.current)
     }
