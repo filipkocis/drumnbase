fa35ac86f1b310ca639d9a2f8f3ce9f4181fa235
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-09-10 22:04:47 +0200

    feat(runner): implement statement eval

---
+++ src/syntax/runner/statement.rs
@@ -0,0 +1,62 @@
+use crate::{syntax::ast::{Statement, Node}, basics::row::Value};
+
+use super::Runner;
+
+impl Runner {
+    pub(super) fn eval_statement(&self, statement: &Statement) -> Result<Value, String> {
+        match statement {
+            Statement::Assignment { name, value } => self.eval_assignment(name, value),
+            Statement::Expression(_) => unimplemented!("statement expression"),
+            Statement::Function { name, parameters, return_type, block } 
+                => self.eval_function(name, parameters, return_type, block),     
+            Statement::Let { name, value } => self.eval_declaration(name, value),
+            Statement::If { condition, then_block, else_block } 
+                => self.eval_if(condition, then_block, else_block),
+            Statement::While { condition, block } => self.eval_while(condition, block),
+            Statement::For { initializer, condition, action, block }
+                => self.eval_for(initializer, condition, action, block),
+            Statement::Loop { block } => self.eval_loop(block),
+
+            Statement::Return(_) => Err("Return outside of function".to_string()),
+            Statement::Break => Err("Break outside of loop".to_string()),
+            Statement::Continue => Err("Continue outside of loop".to_string()),
+        }
+    }
+    fn eval_if(&self, condition: &Node, then_block: &Box<Node>, else_block: &Option<Box<Node>>) -> Result<Value, String> {
+        if !matches!(condition, Node::Expression(_)) {
+            return Err("If condition must be an expression".to_string())
+        }
+
+        let condition = self.run(condition)?;
+        if let Value::Boolean(true) = condition {
+            self.run(then_block)
+        } else {
+            if let Some(else_block) = else_block {
+                self.run(else_block)
+            } else {
+                Ok(Value::Null)
+            }
+        }
+    }
+
+    fn eval_declaration(&self, name: &str, value: &Node) -> Result<Value, String> {
+        let value = self.run(value)?;
+        let mut variables = self.variables.borrow_mut();
+        if variables.contains_key(name) {
+            Err(format!("Variable '{}' already exists", name))
+        } else {
+            variables.insert(name.to_string(), value);
+            Ok(Value::Null)
+        }
+    }
+
+    pub(crate) fn eval_assignment(&self, name: &str, value: &Node) -> Result<Value, String> {
+        let value = self.run(value)?;
+        let mut variables = self.variables.borrow_mut();
+        if let None = variables.get_mut(name).map(|v| *v = value) {
+            Err(format!("Variable '{}' not found", name))
+        } else {
+            Ok(Value::Null)
+        }
+    }
+}
