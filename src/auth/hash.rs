3e65798fe8143c441976c2ac1b328fa4fddfa21c
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-09-29 00:44:18 +0200

    doc(hash): add doc comments

--- src/auth/hash.rs
+++ src/auth/hash.rs
@@ -9,6 +9,7 @@ use argon2::{
 pub struct Hashish;
 
 impl Hashish {
+    /// Hash a password, returns a PHC string
     pub fn hash(password: &str) -> Result<String, String> {
         let salt = SaltString::generate(&mut OsRng);
         let password_hash = Argon2::default()
@@ -18,6 +19,7 @@ impl Hashish {
         Ok(password_hash)
     }
 
+    /// Verify a password against a PHC string
     pub fn verify(password: &str, hash: &str) -> Result<bool, String> {
         let password_hash = PasswordHash::new(hash).map_err(|e| e.to_string())?;
         let verified = Argon2::default()
