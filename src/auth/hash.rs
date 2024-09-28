ee368d3ce60242d5ad97d9055be68d4bb8eb5842
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-09-29 00:33:12 +0200

    feat(hash): add hashing dep, implement pass hasher

---
+++ src/auth/hash.rs
@@ -0,0 +1,29 @@
+use argon2::{
+    password_hash::{
+        rand_core::OsRng,
+        PasswordHash, PasswordHasher, PasswordVerifier, SaltString
+    },
+    Argon2
+};
+
+pub struct Hashish;
+
+impl Hashish {
+    pub fn hash(password: &str) -> Result<String, String> {
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = Argon2::default()
+            .hash_password(password.as_bytes(), &salt).map_err(|e| e.to_string())?
+            .to_string();
+
+        Ok(password_hash)
+    }
+
+    pub fn verify(password: &str, hash: &str) -> Result<bool, String> {
+        let password_hash = PasswordHash::new(hash).map_err(|e| e.to_string())?;
+        let verified = Argon2::default()
+            .verify_password(password.as_bytes(), &password_hash)
+            .is_ok();
+
+        Ok(verified)
+    }
+}
--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,7 +1,9 @@
 mod privilege;
 mod role;
 mod user;
+mod hash;
 
 pub use privilege::*;
 pub use role::Role;
 pub use user::User;
+pub use hash::Hashish;
