df5df2c692b2de4786802c549c37a8fd099a6834
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-09-28 19:28:36 +0200

    feat(user): add new method, add hash field

--- src/auth/user.rs
+++ src/auth/user.rs
@@ -2,7 +2,24 @@ use super::{Privilege, Role};
 
 pub struct User {
     pub name: String,
+    pub hash: String,
     pub roles: Vec<Role>,
     pub privileges: Vec<Privilege>,
     pub is_superuser: bool,
+}
+
+impl User {
+    pub fn new(name: &str, hash: &str) -> Self {
+        Self {
+            name: name.to_owned(),
+            hash: hash.to_owned(),
+            roles: Vec::new(),
+            privileges: Vec::new(),
+            is_superuser: false,
+        }
+    }
+
+    pub fn add_role(&mut self, role: Role) {
+        self.roles.push(role);
+    }
 }
