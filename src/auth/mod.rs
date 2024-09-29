0f7e0706379d9377cb6e1fd646a9bca3e14bcad7
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-09-29 18:47:38 +0200

    feat(authenticate): impl auth for cluster

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -2,8 +2,35 @@ mod privilege;
 mod role;
 mod user;
 mod hash;
+mod authorize;
 
 pub use privilege::*;
 pub use role::Role;
 pub use user::User;
 pub use hash::Hashish;
+pub use authorize::Authorize;
+
+use crate::cluster::Cluster;
+
+use self::action::DatabaseAction;
+
+impl Cluster {
+    /// Authenticate a user with username and password on a database
+    pub fn authenticate(&self, username: &str, password: &str, database: &str) -> Result<User, String> {
+        let user = self.users.get(username).ok_or("user not found")?;
+        let verified = Hashish::verify(password, &user.hash)?;
+        
+        let database = match self.databases.get(database) {
+            Some(database) => database.clone(),
+            None => return Err("database not found".to_string())
+        };
+
+        database.read().unwrap().authorize(user, DatabaseAction::Connect)?;
+
+        if verified {
+            Ok(user.clone())
+        } else {
+            Err("could not authenticate".to_owned())
+        }
+    }
+}
