da1ffaa627d363a51d8a40e747a8bc6b09e3b04a
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-09-29 18:45:57 +0200

    feat(has_privilege): impl trait for user and role

---
+++ src/auth/privilege/has_privilege.rs
@@ -0,0 +1,26 @@
+use crate::auth::{User, Role};
+
+use super::{Privilege};
+
+pub trait HasPrivilege {
+    fn has_privilege(&self, privilege: &Privilege) -> bool;
+    fn has_privileges(&self, privilege: &[Privilege]) -> bool {
+        privilege.iter().all(|p| self.has_privilege(p))
+    }
+}
+
+impl HasPrivilege for User {
+    fn has_privilege(&self, privilege: &Privilege) -> bool {
+        if self.privileges.iter().any(|p| p == privilege) {
+            return true
+        }
+
+        self.roles.iter().any(|role| role.has_privilege(privilege))
+    }
+}
+
+impl HasPrivilege for Role {
+    fn has_privilege(&self, privilege: &Privilege) -> bool {
+        self.privileges.iter().any(|p| p == privilege)
+    }
+}
