1fd7f7d2520fbe8e819f6808ed982b7ea2de73d6
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-09-29 18:47:15 +0200

    feat(authorize): impl authorize trait for priv objects

---
+++ src/auth/authorize.rs
@@ -0,0 +1,46 @@
+use crate::{database::Database, basics::{Table, Column}, function::Function};
+
+use super::{User, PrivilegeFromObject, HasPrivilege};
+
+pub trait Authorize {
+    /// Authorize a user to perform an action on the object
+    fn authorize(&self, user: &User, action: <Self as PrivilegeFromObject>::Action) -> Result<(), String>
+        where Self: PrivilegeFromObject
+    {
+        if user.is_superuser {
+            return Ok(())
+        }
+
+        let privilege = self.privilege_for(action);
+        let authorized = user.has_privilege(&privilege);
+
+        if authorized {
+            Ok(())
+        } else {
+            Err("unauthorized".to_owned())
+        }
+    }
+
+    /// Authorize a user to perform multiple actions on the object
+    fn authorize_all(&self, user: &User, actions: &[<Self as PrivilegeFromObject>::Action]) -> Result<(), String>
+        where Self: PrivilegeFromObject
+    {
+        if user.is_superuser {
+            return Ok(())
+        }
+
+        let privileges: Vec<_> = actions.iter().map(|action| self.privilege_for(action.clone())).collect();
+        let authorized = user.has_privileges(&privileges);
+
+        if authorized {
+            Ok(())
+        } else {
+            Err("unauthorized".to_owned())
+        }
+    }
+}
+
+impl Authorize for Database {}
+impl Authorize for Table {}
+impl Authorize for Column {}
+impl Authorize for Function {}
