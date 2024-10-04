41625538ef2db56c4071247434a4a4ead235e3a8
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-04 02:06:01 +0200

    feat(utils): add fn is_valid_name

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,14 @@
 pub mod disk;
 pub mod log;
 pub mod args;
+
+pub fn is_valid_name(name: &str) -> bool {
+    if name.is_empty() ||
+        name.len() > 64 ||
+        name.len() < 3 ||
+        name.chars().next().unwrap().is_ascii_digit() {
+        return false
+    }
+
+    name.chars().all(|c| c.is_ascii_alphanumeric() || c == '_')
+}
