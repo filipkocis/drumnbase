a0b7d7e958296024d2fff86700069f853ab2c2ac
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-04 15:22:29 +0200

    refactor(database): move code into submodules

--- src/database/creator.rs
+++ src/database/creator.rs
@@ -1,53 +1,10 @@
-use std::{collections::HashMap, fs::File, io::Write};
+use std::io::Write;
 
-use crate::{basics::table::Table, function::Function, syntax::{ast::{Node, SDL, CreateSDL}, stringify::ToSchemaString}, utils::{disk, is_valid_name}, auth::RlsPolicy};
+use crate::{basics::table::Table, syntax::{ast::{Node, SDL, CreateSDL}, stringify::ToSchemaString}, utils::{disk, is_valid_name}, auth::RlsPolicy};
 
-#[derive(Debug)]
-pub struct Database {
-    pub name: String,    
-    pub tables: Vec<Table>,
-    pub root_dir: String,
-    // pub views: Vec<View>,
-    pub functions: HashMap<String, Function>,
-    // pub procedures: Vec<Procedure>,
-    // pub triggers: Vec<Trigger>,
-    // pub indexes: Vec<Index>,
-    // pub sequences: Vec<Sequence>,
-    // pub roles: Vec<Role>,
-    // pub users: Vec<User>,
-    // pub groups: Vec<Group>,
-    // pub privileges: Vec<Privilege>,
-    // pub constraints: Vec<Constraint>,
-    // pub schemas: Vec<Schema>,
-    pub(super) schema: File,
-}
-
-// impl Default for Database {
-//     fn default() -> Self {
-//         Database {
-//             name: String::from(""),
-//             tables: vec![],
-//             root_dir: String::from("data"),
-//             functions: HashMap::new(),
-//
-//             schema: File::new
-//         }
-//     } 
-// }
+use super::Database;
 
 impl Database {
-    pub fn path(&self) -> String {
-        format!("{}/{}", self.root_dir, self.name) 
-    }
-
-    pub fn get_table(&self, name: &str) -> Option<&Table> {
-        self.tables.iter().find(|table| table.name == name)
-    }
-
-    pub fn get_table_mut(&mut self, name: &str) -> Option<&mut Table> {
-        self.tables.iter_mut().find(|table| table.name == name)
-    }
-
     /// Create a new physical table in the database, and update the schema file
     pub fn create_table(&mut self, table: Table) -> Result<(), String> {
         if self.tables.iter().any(|t| t.name == table.name) {
--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,8 +1,46 @@
-mod database;
 mod new;
 mod builder;
 mod run;
+mod loader;
+mod creator;
 
-pub use database::Database;
 pub use builder::DatabaseBuilder;
 pub use run::{QueryResult, Run, RunOptions};
+
+use std::{collections::HashMap, fs::File};
+
+use crate::{basics::Table, function::Function};
+
+#[derive(Debug)]
+pub struct Database {
+    pub name: String,    
+    pub tables: Vec<Table>,
+    pub root_dir: String,
+    // pub views: Vec<View>,
+    pub functions: HashMap<String, Function>,
+    // pub procedures: Vec<Procedure>,
+    // pub triggers: Vec<Trigger>,
+    // pub indexes: Vec<Index>,
+    // pub sequences: Vec<Sequence>,
+    // pub roles: Vec<Role>,
+    // pub users: Vec<User>,
+    // pub groups: Vec<Group>,
+    // pub privileges: Vec<Privilege>,
+    // pub constraints: Vec<Constraint>,
+    // pub schemas: Vec<Schema>,
+    schema: File,
+}
+
+impl Database {
+    pub fn path(&self) -> String {
+        format!("{}/{}", self.root_dir, self.name) 
+    }
+
+    pub fn get_table(&self, name: &str) -> Option<&Table> {
+        self.tables.iter().find(|table| table.name == name)
+    }
+
+    pub fn get_table_mut(&mut self, name: &str) -> Option<&mut Table> {
+        self.tables.iter_mut().find(|table| table.name == name)
+    }
+}
