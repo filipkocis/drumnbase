d68e44dd80cd59a7be11282c11cf3265b6b09749
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-05 15:31:11 +0200

    feat(internaldb): implement new internal db schema

--- src/cluster/builder/mod.rs
+++ src/cluster/builder/mod.rs
@@ -32,4 +32,36 @@ impl ClusterBuilder {
 
     pub const INTERNAL_DB_NAME: &'static str = "drumnbase";
     pub const INTERNAL_SUPERUSER_NAME: &'static str = "drumnbase";
+    pub const INTERNAL_DB_SCHEMA: &'static str = r#"
+        create table users {
+            id: u64, unique, required, default(seq("users", "id"));
+            name: fixed(64), unique, required;
+            hash: fixed(200), unique, required;
+            is_superuser: bool, required, default(false);
+            created_at: time(ms), required, default(now());
+        };
+
+        create table roles {
+            id: u64, unique, required, default(seq("roles", "id"));
+            name: fixed(64), unique, required;
+            description: fixed(200);
+            created_at: time(ms), required, default(now());
+        };
+
+        create table user_roles {
+            id: u64, unique, required, default(seq("user_roles", "id"));
+            user_id: u64, required;
+            role_id: u64, required;
+            created_at: time(ms), required, default(now());
+        };
+
+        create table privileges {
+            id: u64, unique, required, default(seq("privileges", "id"));
+            role_id: u64, required;
+            object: fixed(64), required;
+            object_name: fixed(64);
+            action: fixed(64), required;
+            extra: fixed(64);
+        };
+    "#;
 }
