4856994691724eadeb714ac394f700c7d4e9ffe4
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-05 15:37:33 +0200

    feat(database): implement db::new and db::extract

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -4,10 +4,11 @@ mod run;
 mod loader;
 mod creator;
 
-pub use builder::DatabaseBuilder;
+// pub use builder::DatabaseBuilder;
+pub use loader::DatabaseBuilder;
 pub use run::{QueryResult, Run, RunOptions};
 
-use std::{collections::HashMap, fs::File};
+use std::{collections::HashMap, fs::File, sync::{Arc, RwLock}};
 
 use crate::{basics::Table, function::Function};
 
@@ -32,6 +33,36 @@ pub struct Database {
 }
 
 impl Database {
+    /// Return new Database instance
+    pub fn new(name: &str, root_dir: &str, schema: File) -> Self {
+        Self {
+            name: name.to_string(),
+            tables: Vec::new(),
+            root_dir: root_dir.to_string(),
+            functions: HashMap::new(),
+            schema,
+        }
+    }
+
+    /// Extract the database from Arc<RwLock<Database>>,
+    /// used during internal loading processes.
+    ///
+    /// # Safety
+    /// Provided database should not have any active clones, read or write locks.
+    /// It also should not be used after the extraction
+    pub fn extract(database: Arc<RwLock<Self>>) -> Result<Self, String> {
+        let mut db = database.write().unwrap();
+        let database = Database {
+            name: db.name.clone(),
+            root_dir: db.root_dir.clone(),
+            schema: db.schema.try_clone().or_else(|e| Err(e.to_string()))?,
+            tables: db.tables.drain(..).collect(), 
+            functions: db.functions.drain().collect(),
+        };
+
+        Ok(database)
+    }
+
     pub fn path(&self) -> String {
         format!("{}/{}", self.root_dir, self.name) 
     }
