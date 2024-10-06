763d18944d2dd54152747732d5fe421c7e973e69
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-06 23:38:23 +0200

    feat(member): implement member runner

--- src/syntax/runner/expression.rs
+++ src/syntax/runner/expression.rs
@@ -11,6 +11,7 @@ impl Runner {
             Expression::Call { name, arguments } => self.eval_call(name, arguments, ctx),
             Expression::Literal(value) => self.eval_literal(value, ctx),
             Expression::Index { object, index } => self.eval_index(object, index, ctx),
+            Expression::Member { object, member } => self.eval_member(object, member, ctx),
 
             _ => unimplemented!("expression")
         }
@@ -23,17 +24,32 @@ impl Runner {
         if let Value::Array(array) = object {
             let index = match index {
                 Value::Numeric(numeric) => numeric.to_i128(), 
-                _ => return Err("Invalid index".to_string())
+                _ => return Err(format!("Array cannot be indexed with {:?}", self.get_type(&index)))
             };
 
             if index < 0 || index >= array.len() as i128 || index >= usize::MAX as i128 {
-                return Err(format!("Index '{}' out of bounds", index))
+                return Err(format!("Index '{}' out of bounds, array length is {}", index, array.len()))
             }
 
             let value = array.into_iter().nth(index as usize).unwrap();
             return Ok(Some(value))
         }
 
-        Err(format!("Object of type {:?} is not indexable", self.get_type(&object)))
+        Err(format!("Value of type {:?} cannot be indexed", self.get_type(&object)))
+    }
+
+    fn eval_member(&self, object: &Box<Node>, member: &str, ctx: &Ctx) -> RunnerResult {
+        let object = self.run(object, ctx)?.ok_or("Object cannot be a statement with no return value")?;
+
+        // TODO: Implement objects
+        // if let Value::Object(object) = object {
+        //     if let Some(value) = object.get(member) {
+        //         return Ok(Some(value.clone()))
+        //     }
+        //
+        //     return Err(format!("Member '{}' not found in object", member))
+        // }
+
+        Err(format!("Value of type {:?} cannot be accessed by member notation", self.get_type(&object)))
     }
 }
